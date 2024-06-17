268ab4da36c69b15b485424c77b367316b073ea3
amirhossein dezhamkhah <dezhamkhah@gmail.com>
2024-06-17 10:40:51 +0330

    add send email

--- config/settings.py
+++ config/settings.py
@@ -131,7 +131,14 @@ AUTHENTICATION_BACKENDS = [
 
 ]
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'amirdezham2002@gmail.com'
+EMAIL_HOST_PASSWORD = 'vhlt bxhf duqg rmnj'
+EMAIL_USE_TLS = True
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
